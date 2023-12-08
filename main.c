1a2e147607d91ce5aebaa550df0948b99de171e2
eyadhashim <hashimeyad648@gmail.com>
2023-12-08 11:24:10 -0800

    0

---
+++ main.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @ac: counts arg number
+ * @argv: array of string
+ * Return: 0 in success and -1 in fail
+ */
+int main(int ac, char **argv)
+{
+	char *line = NULL, **tokens = NULL, **tokenizer = NULL;
+	int status = 0, ind = 0, i;
+	(void) ac;
+
+	while (1)
+	{
+		line = read_line();
+		if (line == NULL)
+			return (status);
+		comment(line);
+		if (is_empty(line))
+		{
+			free(line), status = 0;
+			continue;
+		}
+		ind++;
+		tokenizer = spilt_line(line, ";");
+		if (tokenizer == NULL)
+			continue;
+		for (i = 0; tokenizer[i] != NULL; i++)
+		{
+			tokens = spilt_line(tokenizer[i], " \n\t");
+			if (tokens == NULL)
+			{
+				free(tokens);
+				continue;
+			}
+			if (check_built(line))
+			{
+				handle_built(tokens, status, line, ind, argv, tokenizer);
+				continue;
+			}
+			status = exec(tokens, line, ind, argv);
+			free(tokens);
+		}
+		free(tokenizer), free(line);
+	}
+	return (status);
+}
+
